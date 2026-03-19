fbdc256ba8b82582139c7c82508741eec658ab7e
Zandermelon <Zandercbrooks@gmail.com>
2026-03-19 02:45:22 -0700

    make data folder for other files to use

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from crop_animal import crop
 from build_profile import build_profile
 
 def main():
+    os.makedirs("data", exist_ok=True) # create data folder (other files dependant on it)
     crop() # Crops animal photos
     build_profile() # build embedding profile for animal
 
