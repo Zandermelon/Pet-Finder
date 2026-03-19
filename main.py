1acfe42849c49f70d1bfdfc928dc73c3ced421bc
Zandermelon <Zandercbrooks@gmail.com>
2026-03-19 02:21:03 -0700

    added main for calling all classes and creating pipeline

---
+++ main.py
@@ -0,0 +1,9 @@
+from crop_animal import crop
+from build_profile import build_profile
+
+def main():
+    crop() # Crops animal photos
+    build_profile() # build embedding profile for animal
+
+if __name__ == "__main__":
+    main()
